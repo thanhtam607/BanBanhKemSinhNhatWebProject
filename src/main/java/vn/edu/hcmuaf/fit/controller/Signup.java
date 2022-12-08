7cc88650ac3d65a8f84eb1828a471c5554272361
thanhtam607 <20130396@st.hcmuaf.edu.vn>
2022-12-08 14:45:57 +0700

    Xóa class AccountService

--- src/main/java/vn/edu/hcmuaf/fit/controller/Signup.java
+++ src/main/java/vn/edu/hcmuaf/fit/controller/Signup.java
@@ -1,9 +1,5 @@
 package vn.edu.hcmuaf.fit.controller;
 
-import vn.edu.hcmuaf.fit.db.DBConnect;
-import vn.edu.hcmuaf.fit.model.Account;
-import vn.edu.hcmuaf.fit.service.AccountService;
-
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -23,7 +19,7 @@ public class Signup extends HttpServlet {
         if(!pass.equals(repass)){
             request.getRequestDispatcher("signup.jsp").forward(request,response);
         }
-        AccountService.register(new Account(user,pass));
+
         response.sendRedirect("index.jsp");
     }
 
