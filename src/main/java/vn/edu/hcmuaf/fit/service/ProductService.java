ced474dc10c5b4355001050b1342a858c30f32f3
Mai-Thanh-Thuan-15-4-2 <20130127@st.hcmuaf.edu.vn>
2022-12-10 03:41:02 +0700

    Ket noi DB trang chu hien thi san pham ban chay, san pham moi

--- src/main/java/vn/edu/hcmuaf/fit/service/ProductService.java
+++ src/main/java/vn/edu/hcmuaf/fit/service/ProductService.java
@@ -58,6 +58,84 @@ public class ProductService {
         }
         return null;
     }
+    public static List<Product> sanPhamBanChay(){
+        List<Product> list= new LinkedList<Product>();
+        Statement statement = DBConnect.getInstall().get();
+        Statement stmt = DBConnect.getInstall().get();
+        Statement stmt1 = DBConnect.getInstall().get();
+        if(statement != null)
+            try{
+                ResultSet rs =  statement.executeQuery("SELECT sanpham.MaSP, sanpham.TenSP, sanpham.MaLB, sanpham.KichThuoc, sanpham.KhoiLuong, sanpham.MoTa, sanpham.NoiDung, sanpham.Gia FROM sanpham, cthd WHERE sanpham.MaSP = cthd.MASP GROUP BY MASP ORDER BY COUNT(CTHD.MASP) DESC, cthd.SL DESC LIMIT 5;");
+                while(rs.next()){
+                    ResultSet rsImg = stmt.executeQuery("SELECT anhsp.MaSP,anhsp.Anh from anhsp");
+                    List<String> listImg = new LinkedList<String>();
+                    ResultSet rsCmt = stmt1.executeQuery("SELECT MaSP, TAIKHOAN.TENTK,BinhLuan,NgayBL from Comments, TAIKHOAN where TAIKHOAN.ID = Comments.ID");
+                    List<Comment> listCmts = new LinkedList<Comment>();
+                    String s1 = rs.getString(1);
+                    while (rsImg.next()){
+
+                        String s2 = rsImg.getString(1);
+                        if (s1.equals(s2)) {
+                            listImg.add(rsImg.getString(2));
+                        }
+                    }
+                    while (rsCmt.next()){
+                        String s2 = rsCmt.getString(1);
+                        if(s1.equals(s2)){
+                            listCmts.add(new Comment(rsCmt.getString(1), rsCmt.getString(2), rsCmt.getString(3), rsCmt.getString(4)));
+                        }
+                    }
+                    Product p = new Product(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7),listImg, rs.getInt(8),listCmts);
+                    list.add(p);
+                }
+            }
+            catch (SQLException e){
+                throw new RuntimeException(e);
+            }
+        else{
+            System.out.println("Không có sản phẩm");
+        }
+        return  list;
+    }
+    public static List<Product> sanPhamMoi(){
+        List<Product> list= new LinkedList<Product>();
+        Statement statement = DBConnect.getInstall().get();
+        Statement stmt = DBConnect.getInstall().get();
+        Statement stmt1 = DBConnect.getInstall().get();
+        if(statement != null)
+            try{
+                ResultSet rs =  statement.executeQuery("SELECT sanpham.MaSP, sanpham.TenSP, sanpham.MaLB, sanpham.KichThuoc, sanpham.KhoiLuong, sanpham.MoTa, sanpham.NoiDung, sanpham.Gia FROM sanpham ORDER BY MaSP DESC LIMIT 8;");
+                while(rs.next()){
+                    ResultSet rsImg = stmt.executeQuery("SELECT anhsp.MaSP,anhsp.Anh from anhsp");
+                    List<String> listImg = new LinkedList<String>();
+                    ResultSet rsCmt = stmt1.executeQuery("SELECT MaSP, TAIKHOAN.TENTK,BinhLuan,NgayBL from Comments, TAIKHOAN where TAIKHOAN.ID = Comments.ID");
+                    List<Comment> listCmts = new LinkedList<Comment>();
+                    String s1 = rs.getString(1);
+                    while (rsImg.next()){
+
+                        String s2 = rsImg.getString(1);
+                        if (s1.equals(s2)) {
+                            listImg.add(rsImg.getString(2));
+                        }
+                    }
+                    while (rsCmt.next()){
+                        String s2 = rsCmt.getString(1);
+                        if(s1.equals(s2)){
+                            listCmts.add(new Comment(rsCmt.getString(1), rsCmt.getString(2), rsCmt.getString(3), rsCmt.getString(4)));
+                        }
+                    }
+                    Product p = new Product(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7),listImg, rs.getInt(8),listCmts);
+                    list.add(p);
+                }
+            }
+            catch (SQLException e){
+                throw new RuntimeException(e);
+            }
+        else{
+            System.out.println("Không có sản phẩm");
+        }
+        return  list;
+    }
     public static  void addComment(Comment cmt, String IDUser){
         Statement statement = DBConnect.getInstall().get();
 
