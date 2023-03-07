46231151d314c7e5d609bcd410328b72710241c9
Mai-Thanh-Thuan-15-4-2 <20130127@st.hcmuaf.edu.vn>
2023-03-07 21:50:16 +0700

    them chuc nang sap xep cho cac danh sach trong admin

--- src/main/java/vn/edu/hcmuaf/fit/controller/admin/Product/ListProduct_Admin.java
+++ src/main/java/vn/edu/hcmuaf/fit/controller/admin/Product/ListProduct_Admin.java
@@ -1,14 +1,14 @@
 package vn.edu.hcmuaf.fit.controller.admin.Product;
 
-import vn.edu.hcmuaf.fit.model.Blog;
 import vn.edu.hcmuaf.fit.model.Product;
-import vn.edu.hcmuaf.fit.service.BlogService;
 import vn.edu.hcmuaf.fit.service.ProductService;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @WebServlet(name = "ListProduct_Admin", value = "/admin/ListProduct_Admin")
@@ -17,6 +17,31 @@ public class ListProduct_Admin extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         List<Product> list = ProductService.getListProductForAdmin();
         request.setAttribute("listpro", list);
+        String sort = request.getParameter("sortValue");
+        if (sort != null) {
+            if (sort.equals("Sắp xếp theo tên")) {
+                Collections.sort(list, new Comparator<Product>() {
+                    @Override
+                    public int compare(Product o1, Product o2) {
+                        return o1.getName().compareTo(o2.getName());
+                    }
+                });
+            }
+            if (sort.equals("Sắp xếp theo loại")) {
+                Collections.sort(list, new Comparator<Product>() {
+                    @Override
+                    public int compare(Product o1, Product o2) {
+                        return o1.getType().compareTo(o2.getType());
+                    }
+                });
+            }
+            if (sort.equals("Giá từ cao đến thấp")) {
+                list.sort((Product o1, Product o2) -> o2.getPrice() - o1.getPrice());
+            }
+            if (sort.equals("Giá từ thấp đến cao")) {
+                list.sort((Product o1, Product o2) -> o1.getPrice() - o2.getPrice());
+            }
+        }
 
         String numPage = request.getParameter("page");
         if(numPage == null || numPage.equals("0")){
@@ -36,10 +61,12 @@ public class ListProduct_Admin extends HttpServlet {
         request.getRequestDispatcher("list-products.jsp").forward(request,response);
 
     }
-
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
+    public static void main(String[] args) {
+
+    }
 }
+
